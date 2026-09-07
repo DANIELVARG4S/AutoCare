118ae98b15f2c8b2cc062fc27ce5a16b2da4c72d
Orion <danniliho@gmail.com>
2026-09-06 20:07:57 -0600

    componente input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,3 +1,19 @@
-export default function Input() {
-  return null;
+import React from "react";
+import { Text, TextInput, TextInputProps, View } from "react-native";
+
+interface InputProps extends TextInputProps {
+  label: string;
+  className?: string;
 }
+
+export const Input = ({ label, className, ...textInputProps }: InputProps) => {
+  return (
+    <View className="mb-3">
+      <Text className="mb-1 ml-4 text-gray-700">{label}</Text>
+      <TextInput
+        {...textInputProps}
+        className={`rounded-2xl bg-gray-100 p-4 text-gray-700 ${className ?? ""}`}
+      />
+    </View>
+  );
+};
